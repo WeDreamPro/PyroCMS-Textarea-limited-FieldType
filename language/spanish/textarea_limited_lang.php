2b323d760220ad2fd044291ca828140423ae2ba7
Rigo B Castro <rigobcastro@gmail.com>
2014-02-14 16:40:30 -0500

    Initial commit

---
+++ language/spanish/textarea_limited_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['streams:textarea_limited.name'] = 'Area de texto con límite';
+$lang['streams:textarea_limited.default_text'] = 'Default Text'; #translate
+$lang['streams:textarea_limited.allow_tags'] = 'Allow Lex Tags'; #translate
+$lang['streams:textarea_limited.content_type'] = 'Content Type'; #translate
+$lang['streams:textarea_limited.character_limit'] = 'Limite de carácteres';
+
+$lang['streams:textarea_limited.info_characters'] = 'Caracteres';
+$lang['streams:textarea_limited.info_characters_left'] = 'Caracteres restantes';
+$lang['streams:textarea_limited.info_words'] = 'Palabras';
+
+/* End of file textarea_limited_lang.php */
