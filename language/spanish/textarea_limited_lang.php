53abf0fec20aac182da442d90c6131e1e599b37e
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2014-04-15 08:28:04 -0500

    Translate and Readme

--- language/spanish/textarea_limited_lang.php
+++ language/spanish/textarea_limited_lang.php
@@ -3,9 +3,9 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 $lang['streams:textarea_limited.name'] = 'Area de texto con límite';
-$lang['streams:textarea_limited.default_text'] = 'Default Text'; #translate
-$lang['streams:textarea_limited.allow_tags'] = 'Allow Lex Tags'; #translate
-$lang['streams:textarea_limited.content_type'] = 'Content Type'; #translate
+$lang['streams:textarea_limited.default_text'] = 'texto por defecto'; #translate
+$lang['streams:textarea_limited.allow_tags'] = 'Permitir Tags de Lex'; #translate
+$lang['streams:textarea_limited.content_type'] = 'Tipo de contenido'; #translate
 $lang['streams:textarea_limited.character_limit'] = 'Limite de carácteres';
 
 $lang['streams:textarea_limited.info_characters'] = 'Caracteres';
